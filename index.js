71dcf439c1aea5c417abbce0a0d5a4643de82a14
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-09-23 16:40:13 -0400

    edits

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 //import express and axios 
 const express = require('express')
 const axios = require('axios')
+//import and config dotenv library
+require('dotenv').config()
+
 const PORT = 4000
+//define API key var
+const omdbApiKey = process.env.OMDB_API_KEY  //how to access secured api key specifically through express
 
 //declare an instance of app
 const app = express()
 
 //create a home route
 app.get('/', (req, res) => {
-    axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=')
+    axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${omdbApiKey}`)
         .then((response) => {
             console.log(response.data)
             res.send(response.data)
